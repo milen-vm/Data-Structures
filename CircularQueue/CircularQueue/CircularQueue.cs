d0d0f6049239d73819afe3c23727d0247a3e439f
milen-vm <bay_milen@abv.bg>
2015-07-21 01:00:02 +0300

    Add Circular Queue lab

---
+++ CircularQueue/CircularQueue/CircularQueue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class CircularQueue<T>
+{
+    private const int initialCapacity = 16;
+
+    private T[] elements;
+    private int startIndex = 0;
+    private int endIndex = 0;
+
+    public int Count { get; private set; }
+
+    public CircularQueue() : this(initialCapacity)
+    {
+    }
+
+    public CircularQueue(int capacity)
+    {
+        this.elements = new T[capacity];
+    }
+
+    public void Enqueue(T element)
+    {
+        if (this.Count == this.elements.Length)
+        {
+            this.Grow();
+        }
+
+        this.elements[this.endIndex] = element;
+        this.endIndex = (this.endIndex + 1) % this.elements.Length;
+        this.Count++;
+    }
+
+    private void Grow()
+    {
+        var newElements = new T[2 * this.elements.Length];
+        this.CopyAllElementsTo(newElements);
+        this.elements = newElements;
+        this.startIndex = 0;
+        this.endIndex = this.Count;
+    }
+
+    private void CopyAllElementsTo(T[] resultArr)
+    {
+        int sourceIndex = this.startIndex;
+        int destinationIndex = 0;
+        for (int i = 0; i < this.Count; i++)
+        {
+            resultArr[destinationIndex] = this.elements[sourceIndex];
+            sourceIndex = (sourceIndex + 1) % this.elements.Length;
+            ++destinationIndex;
+        }
+    }
+
+    public T Dequeue()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("The queue is empty!");
+        }
+
+        var result = this.elements[this.startIndex];
+        this.startIndex = (this.startIndex + 1) % this.elements.Length;
+        this.Count--;
+
+        return result;
+    }
+
+    public T[] ToArray()
+    {
+        var resultArray = new T[this.Count];
+        this.CopyAllElementsTo(resultArray);
+
+        return resultArray;
+    }
+}
