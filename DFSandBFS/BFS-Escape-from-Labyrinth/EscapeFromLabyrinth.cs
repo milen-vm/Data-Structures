b8d1ae3beef465d4d7228b3ed27be93ec7171df9
milen-vm <milen_vm@abv.bg>
2015-08-08 02:46:08 +0300

    Add Graph Traversal lab

---
+++ DFSandBFS/BFS-Escape-from-Labyrinth/EscapeFromLabyrinth.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+public class EscapeFromLabyrinth
+{
+    const char VisitedCell = 's';
+
+    static int width = 9;
+    static int height = 7;
+    static char[,] labirint = 
+    {
+        {'*', '*', '*', '*', '*', '*', '*', '*', '*'},
+        {'*', '-', '-', '-', '-', '*', '*', '-', '-'},
+        {'*', '*', '-', '*', '-', '-', '-', '-', '*'},
+        {'*', '-', '-', '*', '-', '*', '-', '*', '*'},
+        {'*', 's', '*', '-', '-', '*', '-', '*', '*'},
+        {'*', '*', '-', '-', '-', '-', '-', '-', '*'},
+        {'*', '*', '*', '*', '*', '*', '*', '-', '*'}
+    };
+
+    public static void Main()
+    {
+        string shortestPathToExit = FindShortestPathToExit();
+        if (shortestPathToExit == null)
+        {
+            Console.WriteLine("No exit");
+        }
+        else if (shortestPathToExit == "")
+        {
+            Console.WriteLine("Start is at the exit");
+        }
+        else
+        {
+            Console.WriteLine("Shortest exit: " + shortestPathToExit);
+        }
+    }
+
+    static string FindShortestPathToExit()
+    {
+        var queue = new Queue<Point>();
+        var startPosition = FindStartPosition();
+        if (startPosition == null)
+        {
+            return null;
+        }
+
+        queue.Enqueue(startPosition);
+        while (queue.Count > 0)
+        {
+            var curentCell = queue.Dequeue();
+            if (IsExit(curentCell))
+            {
+                return TracePathBack(curentCell);
+            }
+
+            TryDirection(queue, curentCell, "U", 0, -1);
+            TryDirection(queue, curentCell, "R", 1, 0);
+            TryDirection(queue, curentCell, "D", 0, 1);
+            TryDirection(queue, curentCell, "L", -1, 0);
+        }
+
+        return null;
+    }
+
+    static Point FindStartPosition()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (labirint[y, x] == VisitedCell)
+                {
+                    return new Point() { X = x, Y = y };
+                }
+            }
+        }
+
+        return null;
+    }
+
+    static bool IsExit(Point currentCell)
+    {
+        bool isOnBorderX = currentCell.X == 0 || currentCell.X == width - 1;
+        bool isOnBorderY = currentCell.Y == 0 || currentCell.Y == height - 1;
+
+        return isOnBorderX || isOnBorderY;
+    }
+
+    static void TryDirection(Queue<Point> queue, Point currentCell, 
+        string direction, int deltaX, int deltaY)
+    {
+        int newX = currentCell.X + deltaX;
+        int newY = currentCell.Y + deltaY;
+        if (newX >= 0 && newX < width && newY >= 0 && newY < height && labirint[newY, newX] == '-') {
+            labirint[newY, newX] = VisitedCell;
+            var nextCell = new Point()
+            {
+                X = newX,
+                Y = newX,
+                Direction = direction,
+                PreviousPoint = currentCell
+            };
+
+            queue.Enqueue(nextCell);
+        }
+    }
+
+    static string TracePathBack(Point currentCell)
+    {
+        var path = new StringBuilder();
+        while (currentCell.PreviousPoint != null)
+        {
+            path.Append(currentCell.Direction);
+            currentCell = currentCell.PreviousPoint;
+        }
+
+        var pathReversed = new StringBuilder();
+        for (int i = path.Length - 1; i >= 0; i--)
+        {
+            pathReversed.Append(path[i]);
+        }
+
+        return pathReversed.ToString();
+    }
+}
---
+++ DFSandBFS/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph = new List<int>[]
+    {
+        new List<int>() { 3, 6 },
+        new List<int>() {3, 4, 5, 6 },
+        new List<int>() { 8 },
+        new List<int>() { 0, 1, 5 },
+        new List<int>() { 1, 6 },
+        new List<int>() { 1, 3 },
+        new List<int>() { 0, 1, 4 },
+        new List<int>() {},
+        new List<int>() { 2 },
+    };
+
+    static bool[] visited;
+
+    public static void Main()
+    {
+        
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    static void DFS(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            foreach (var childNode in graph[node])
+            {
+                DFS(childNode);
+            }
+
+            Console.Write(" " + node);
+        }
+    }
+
+    static void FindGraphConnectedComponents()
+    {
+        visited = new bool[graph.Length];
+        for (int startNode = 0; startNode < graph.Length; startNode++)
+        {
+            if (!visited[startNode])
+            {
+                Console.Write("Connected component:");
+                DFS(startNode);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    static List<int>[] ReadGraph()
+    {
+        int graphSize = int.Parse(Console.ReadLine());
+        var graph = new List<int>[graphSize];
+        visited = new bool[graphSize];
+        for (int row = 0; row < graphSize; row++)
+        {
+            graph[row] = new List<int>();
+            string neighboursLine = Console.ReadLine();
+            string[] neighboursSplit = neighboursLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < neighboursSplit.Length; i++)
+            {
+                graph[row].Add(int.Parse(neighboursSplit[i]));
+            }
+        }
+
+        return graph;
+    }
+}
